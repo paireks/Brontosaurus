c3d8be4e43f5f753265ea534c15a9035ebb196aa
W Radaczyński <w.radaczynski@gmail.com>
2020-06-06 20:36:08 +0200

    Build

---
+++ Charonosaurus/AssertGH.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Charonosaurus
+{
+    public class AssertGH : GH_Component
+    {
+        public AssertGH()
+          : base("Assert", "Assert",
+              "If your actual value == expected value: component will pass the test.",
+              "Pterodactyl", "Test")
+        {
+        }
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Test Names",
+                "Test Names",
+                "Test names as list, so it'll be easier to check which test passed/failed",
+                GH_ParamAccess.list);
+            pManager.AddTextParameter("Actual",
+                "Actual",
+                "Actual values that you want to check, as list",
+                GH_ParamAccess.list);
+            pManager.AddTextParameter("Expected",
+                "Expected",
+                "Expected values as list",
+                GH_ParamAccess.list);
+        }
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Result", "Result", "Information about tests, which one passed/failed",
+                GH_ParamAccess.list);
+            pManager.AddTextParameter("ReportPart", "ReportPart",
+                "Created part of the report, you can use it with Pterodactyl plugin to create custom reports",
+                GH_ParamAccess.item);
+        }
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+        }
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("2e57844a-7d1d-4558-aaac-cc00e10cd89b"); }
+        }
+    }
+}
---
+++ CharonosaurusEngineTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CharonosaurusEngineTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
